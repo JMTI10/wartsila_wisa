118855b716c6a2e6fc9ba9b0850ae8c9e0a3540b
JMTI10 <idg@live.com.pt>
2025-09-27 21:21:41 +0300

    Changes to be committed: 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
 import MainChart from './components/MainChart';
-import Sidebar from './components/Sidebar';
-import DataDisplay from './components/DataDisplay';
+import Sidebar from './components/Sidebar'; // MAKE SURE THIS IMPORT EXISTS
 
 function App() {
   return (
     <div className="App">
       <div className="main-layout">
         <MainChart />
-        <Sidebar />
-        <DataDisplay />
+        <Sidebar /> {/* MAKE SURE THIS LINE EXISTS */}
       </div>
     </div>
   );
